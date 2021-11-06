7e5e43ac55dd01a84eee07304e9f01d8becbebc4
Thomas Bittner <thomas.bittner@generic.de>
2021-11-06 12:03:26 +0100

    Add HexLine class

--- 7/HexDump/HexDump/HexConverter.cs
+++ 7/HexDump/HexDump/HexConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -9,61 +10,77 @@ namespace HexDump
 	{
 		public static string ToHexDump(string filePath)
 		{
-      var result = "";
-
-      using (var input = File.OpenRead(filePath))
-      {
-        int position = 0;
-        var buffer = new byte[16];
-
-        while (position < input.Length)
-        {
-          var charsRead = input.Read(buffer, 0, buffer.Length);
-          if (charsRead > 0)
-          {
-            result += PositionToHex(position);
-            result += ": ";
-
-            position += charsRead;
-
-            for (int i = 0; i < 16; i++)
-            {
-              result += (i < charsRead) ? CharacterToHex(buffer[i]) + " " : "  ";
-
-              if (i == 7)
-              {
-                result += "-- ";
-              }
-            }
-
-            result += "  ";
-            result += BufferToString(buffer, charsRead);
-            result += "\n";
-          }
-        }
-      }
-      return result;
-    }
-
-    private static string PositionToHex(int position)
-      => string.Format("{0:x4}", position);
-
-    private static string CharacterToHex(byte character)
-      => string.Format("{0:x2}", character);
-
-    private static string BufferToString(byte[] buffer, int charsRead)
-		{
-      foreach (ref var character in buffer.AsSpan())
-        character = ReplaceExoticCharacters(character);
-      var bufferContent = Encoding.ASCII.GetString(buffer);
-      return bufferContent.Substring(0, charsRead);
+			var hexLines = CreateHexLinesFromFile(filePath);
+			var hexDumpLines = hexLines.Select(line => line.ToHexDump());
+			return string.Join("", hexDumpLines);
 		}
 
-    private static byte ReplaceExoticCharacters(byte character)
-    {
-      if (character < 32 || 250 < character)
-        return (byte)'.';
-      return character;
-    }
-  }
+		private static IEnumerable<HexLine> CreateHexLinesFromFile(string filePath)
+		{
+			using var inputStream = File.OpenRead(filePath);
+			var bufferSize = 16;
+			for (var position = 0; position < inputStream.Length; position += bufferSize)
+			{
+				var buffer = new byte[bufferSize];
+				var charactersRead = inputStream.Read(buffer, 0, buffer.Length);
+				yield return new HexLine
+				{
+					Position = position,
+					Characters = buffer,
+					NumberOfCharacters = charactersRead
+				};
+			}
+		}
+
+		class HexLine
+		{
+			public int Position { get; set; }
+			public byte[] Characters { get; set; }
+			public int NumberOfCharacters { get; set; }
+
+			public string ToHexDump()
+			{
+				var result = PositionToHex(Position);
+				result += ": ";
+				result += CreateHexString();
+				result += "  ";
+				result += BufferToString(Characters, NumberOfCharacters);
+				result += "\n";
+				return result;
+			}
+
+			private string CreateHexString()
+			{
+				var result = "";
+				for (var i = 0; i < Characters.Length; i++)
+				{
+					result += (i < NumberOfCharacters) ? CharacterToHex(Characters[i]) + " " : "  ";
+					if (i == 7)
+						result += "-- ";
+				}
+				return result;
+			}
+		}
+
+		private static string PositionToHex(int position)
+			=> string.Format("{0:x4}", position);
+
+		private static string CharacterToHex(byte character)
+			=> string.Format("{0:x2}", character);
+
+		private static string BufferToString(byte[] buffer, int charsRead)
+		{
+			foreach (ref var character in buffer.AsSpan())
+				character = ReplaceExoticCharacters(character);
+			var bufferContent = Encoding.ASCII.GetString(buffer);
+			return bufferContent.Substring(0, charsRead);
+		}
+
+		private static byte ReplaceExoticCharacters(byte character)
+		{
+			if (character < 32 || 250 < character)
+				return (byte)'.';
+			return character;
+		}
+	}
 }
